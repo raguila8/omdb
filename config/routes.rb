02068d6b07fa07d6a8276f5056aa3353e1538d76
Rodrigo Aguilar <rodrigoaguilar887@gmail.com>
2017-10-25 18:13:40 -0700

    basic bootstrap layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-
-  get 'movies/show'
-
+	root 'movies#index'
+  get '/movie', to: 'movies#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
